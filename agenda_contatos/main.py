6a23843ba1ddeead65f06140dd27f50aee89e5fc
Giovanna Monteiro Lima <giovannamonteiro1303@gmail.com>
2026-01-29 20:36:44 -0300

    Criar novo contato
    Inserida a função de criação de um novo contato.

--- agenda_contatos/main.py
+++ agenda_contatos/main.py
@@ -1,3 +1,27 @@
+agenda = []
+
+#Função de criar novo contato
+def criar_novo_contato(agenda):
+    nome = input("Digite o nome: ")
+    telefone = input("Digite o n° de telefone: ")
+    email = input("Digite o E-mail: ")
+    print("Deseja marcar como favorito?")
+    print("1. Sim")
+    print("2. Não")
+    favorito_sim_nao = input("Escolha a opção desejada: ")
+    favorito = favorito_sim_nao == "1"
+    contato = {
+        "nome": nome,
+        "telefone": telefone,
+        "email": email,
+        "favorito": favorito
+    }
+    agenda.append(contato)
+    print("Contato adicionado com sucesso.")
+
+    return
+
+#Menu de funcionalidades da agenda
 while True:
     print("1. Criar novo contato.")
     print("2. Visualizar contatos existentes.")
@@ -10,5 +34,7 @@ while True:
 
     if opcao_escolhida == "6":
         break
+    elif opcao_escolhida == "1":
+        criar_novo_contato(agenda)
 
 print("Programa finalizado com sucesso.")
