513d3e27398ca259b018cb7724e6e4ef2977dccc
Giovanna Monteiro Lima <giovannamonteiro1303@gmail.com>
2026-01-29 20:11:08 -0300

    Criação e estrutura da agenda
    Elaboração simples da interface via terminal.

---
+++ agenda_contatos/main.py
@@ -0,0 +1,14 @@
+while True:
+    print("1. Criar novo contato.")
+    print("2. Visualizar contatos existentes.")
+    print("3. Editar contato.")
+    print("4. Apagar contato.")
+    print("5. Contatos favoritados.")
+    print("6. Finalizar programa.")
+
+    opcao_escolhida = input("Digite a opção desejada: ")
+
+    if opcao_escolhida == "6":
+        break
+
+print("Programa finalizado com sucesso.")
